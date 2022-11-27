b587c01a5083022d5fdce0cae5f8d7d8ed68e962
AndresCM <q.chavezandres@gmail.com>
2022-11-27 12:43:29 -0700

    Add: Delete task by id, Fix: some bugs in the application

--- index.ts
+++ index.ts
@@ -7,24 +7,6 @@ app.use(express.json());
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-const initTasks = () => {
-  let listOfTask: listOfTasks = {
-    items: [
-      {
-        id: 1,
-        name: "Correr en el parque",
-        elapsed_time: 20,
-        start_time: 1622253787,
-        deleted: false,
-        project_id: 1,
-      },
-    ] as Task[],
-    count: 1,
-    total: 1,
-  };
-  return listOfTask;
-};
-
 const tasks = initTasks();
 
 app.get("/", (req, res) => {
@@ -42,7 +24,7 @@ app.post("/task/create", (req, res) => {
   validateBody(body, res);
 
   const newTask = {
-    id: tasks.items.length + 1,
+    id: getRandomInt(),
     name: body.name,
     elapsed_time: body.elapsed_time,
     start_time: body.start_time,
@@ -69,30 +51,31 @@ app.get("/task/:id", (req, res) => {
   res.status(200).send(taskFound);
 });
 
-//Update task //Check if it works
+//Update task //Working
 app.put("/task/update/:id", (req, res) => {
   const { id } = req.params;
-  const { task } = req.body;
-
   validateId(id, res);
-  validateBody(task, res);
 
-  const taskFound = tasks.items.findIndex((task) => task.id + "" === id);
-  if (!taskFound) return res.status(400).send("No se ha encontrado la tarea");
+  const body = req.body;
+  validateBody(body, res);
+
+  const taskFoundId = tasks.items.findIndex((task) => task.id + "" === id);
+  if (taskFoundId === -1)
+    return res.status(400).send("No se ha encontrado la tarea");
 
   const newTask = {
-    id: tasks.items.length + 1,
-    name: task.name,
-    elapsed_time: task.elapsed_time,
-    start_time: task.start_time,
-    deleted: task.deleted,
-    project_id: task.project_id,
+    id: tasks.items[taskFoundId].id,
+    name: body.name,
+    elapsed_time: body.elapsed_time,
+    start_time: body.start_time,
+    deleted: body.deleted,
+    project_id: body.project_id,
   };
-  tasks.items[taskFound] = newTask;
+  tasks.items[taskFoundId] = newTask;
   res.status(200).send(tasks);
 });
 
-//Delete task by id //Check if it works
+//Delete task by id //Working
 app.delete("/task/delete/:id", (req, res) => {
   const { id } = req.params;
 
@@ -101,25 +84,56 @@ app.delete("/task/delete/:id", (req, res) => {
   const taskFound = tasks.items.find((task) => task.id + "" === id);
   if (!taskFound) return res.status(400).send("No se ha encontrado la tarea");
 
-  const taskDeleted = tasks.items.filter((task) => task.id + "" !== id);
+  const taskFoundId = tasks.items.findIndex((task) => task.id + "" === id);
+  tasks.items.splice(taskFoundId, 1);
 
-  res.status(200).send(taskDeleted);
+  tasks.count = tasks.items.length;
+  tasks.total = tasks.items.length;
+
+  res.status(200).send(tasks);
 });
 
 //Utils
+function initTasks() {
+  let listOfTask: listOfTasks = {
+    items: [
+      {
+        id: getRandomInt(),
+        name: "Correr en el parque",
+        elapsed_time: 20,
+        start_time: 1622253787,
+        deleted: false,
+        project_id: 1,
+      },
+    ] as Task[],
+    count: 1,
+    total: 1,
+  };
+  return listOfTask;
+}
+
 const validateId = (id: string, res: any) => {
   if (!id) return res.status(400).send("No se ha enviado el id");
   if (isNaN(Number(id))) return res.status(400).send("El id no es un número");
 };
 
 const validateBody = (body: any, res: any) => {
-  if (!body) return res.status(400).send("No se ha enviado el body");
-  if (
-    !body.name ||
-    !body.elapsed_time ||
-    !body.start_time ||
-    !body.project_id
-  ) {
-    return res.status(400).send("Envíe todos los campos requeridos");
+  if (!body) {
+    return res.status(400).send("No se ha enviado el body");
+  }
+  if (!body.name) {
+    return res.status(400).send("Envíe el nombre de la tarea");
+  } else if (!body.elapsed_time) {
+    return res.status(400).send("Envíe el tiempo transcurrido");
+  } else if (!body.start_time) {
+    return res.status(400).send("Envíe la hora de inicio");
+  } else if (body.deleted === undefined) {
+    return res.status(400).send("Envíe el estado de eliminado");
+  } else if (!body.project_id) {
+    return res.status(400).send("Envíe el id del proyecto");
   }
 };
+
+function getRandomInt() {
+  return Math.floor(Math.random() * Math.floor(100));
+}
